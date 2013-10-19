64a43532962dd12857990ae80b0fb1a9e0db870e
Aditya Sinha <iadityasinha@gmail.com>
2013-10-19 20:49:46 +0530

    Circular Queue Enhancements

--- CircularQueue/CircularQueue/main.c
+++ CircularQueue/CircularQueue/main.c
@@ -6,71 +6,84 @@
 //  Copyright (c) 2013 Aditya Sinha. All rights reserved.
 //
 #include<stdio.h>
+#include <stdbool.h>
 #define SIZE 5
-void insert();
-void delet();
+void insert(int);
+void delete();
 void display();
-int queue[SIZE], rear=0, front=0, item, count=0;
+int size();
+bool isEmpty();
+int queue[SIZE], rear=0, front=0, item;
 int main(int argc, const char * argv[])
 {
     int ch;
+    
     do
     {
-        printf("\n\n1.\tInsert\n2.\tDelete\n3.\tDisplay\n4.\tExit\n");
+        printf("\n1.\tInsert\n2.\tDelete\n3.\tDisplay\n4.\tExit\n");
         printf("\nEnter your choice: ");
         scanf("%d", &ch);
         switch(ch)
         {
             case 1:
-                insert();
+                printf("\n\nEnter ITEM: ");
+                scanf("%d", &item);
+                insert(item);
                 break;
             case 2:
-                delet();
+                delete();
                 break;
             case 3:
                 display();
                 break;
             case 4:
                 return 0;
-            default: 
+            default:
                 printf("\n\nInvalid choice. Pleasr try again...\n");
         }
     } while(1);
     return 0;
 }
 
-void insert()
+int size()
 {
-    if(count == SIZE)
+    return (SIZE-front+rear)%SIZE;
+}
+
+bool isEmpty()
+{
+    return (front==rear);
+}
+
+void insert(int item)
+{
+    if(size() == SIZE-1)
         printf("\n\nQueue is full.");
     else
     {
-        printf("\n\nEnter ITEM: ");
-        scanf("%d", &item);
         queue[rear]=item;
         rear = (rear+1)%SIZE;
         printf("\n\nItem inserted: %d\n", item);
-        count++;
+        
     }
 }
-void delet()
+void delete()
 {
-    if(count == 0)
+    if(isEmpty())
         printf("\n\nQueue is empty.\n");
     else
     {
         item = queue[front];
         front= (front+1)%SIZE;
         printf("\n\nITEM deleted: %d", item);
-        count--;
-    } 
+    }
 }
 
 void display()
 {
     int i=front,temp=0;
-    temp=count;
-    if(count == 0)
+    temp=size();
+    if(isEmpty() )
         printf("\n\nQueue is empty.\n");
     while (temp!=0) {
         printf("\t %d",queue[i]);
