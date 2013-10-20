06612b719ae6111dd47ed2f2daab73d2cd823191
Aditya Sinha <iadityasinha@gmail.com>
2013-10-20 20:54:45 +0530

    linked list updated with following options 1.	Insert at the beginning 2.	Delete from the beginning 3.	Display 4.	Insert at the end 5.	Delete from the end 6.	Insert at position

--- Singly_Linked_List/Singly_Linked_List/main.c
+++ Singly_Linked_List/Singly_Linked_List/main.c
@@ -12,6 +12,11 @@
 //#pragma pack(1)
 
 void insertAtTheBeginning(int);
+void insertAtTheEnd(int);
+void deleteFromTheBeginning();
+void deleteFromTheEnd();
+void insertItemAtPostion(int, int);
+
 void display();
 struct ListNode{
     int data;
@@ -29,26 +34,43 @@ int main(int argc, const char * argv[])
     // insert code here...
     int ch;
     int item;
+    int position;
     do
     {
-        printf("\n1.\tInsert at the beginning\n2.\tDelete from the beginning\n3.\tDisplay\n4.\tExit\n");
+        printf("\n1.\tInsert at the beginning\n2.\tDelete from the beginning\n3.\tDisplay\n4.\tInsert at the end\n5.\tDelete from the end\n6.\tInsert at position");
         printf("\nEnter your choice: ");
         scanf("%d", &ch);
         switch(ch)
         {
             case 1:
-                printf("\n\nEnter ITEM to be inserted: ");
+                printf("\n\nEnter ITEM to be inserted at the beginning: ");
                 scanf("%d", &item);
                 insertAtTheBeginning(item);
                 break;
             case 2:
-                
+                deleteFromTheBeginning();
                 break;
             case 3:
+                printf("\n\nHere is the list: ");
                 display();
                 break;
             case 4:
-//                return 0;
+                printf("\n\nEnter ITEM to be inserted at the end: ");
+                scanf("%d", &item);
+                insertAtTheEnd(item);
+                break;
+                
+            case 5:
+                deleteFromTheEnd();
+                break;
+                
+            case 6:
+                
+                printf("\n\nEnter Position: ");
+                scanf("%d", &position);
+                printf("\n\nEnter ITEM to be inserted: ");
+                scanf("%d", &item);
+                insertItemAtPostion(item, position);
                 break;
             default:
                 printf("\n\nInvalid choice. Pleasr try again...\n");
@@ -76,6 +98,17 @@ void insertAtTheBeginning(int item)
     head=newNode;
 }
 
+void deleteFromTheBeginning()
+{
+    if (head==NULL) {
+        printf("\nLL is empty, nothing to delete");
+        return;
+    }
+    NODE *temp = head;
+    head=head->next;
+    free(temp);
+}
+
 void display()
 {
     if (head==NULL) {
@@ -87,4 +120,61 @@ void display()
         printf("\n %d",current->data);
         current=current->next;
     }
+}
+
+void insertAtTheEnd(int item)
+{
+    NODE *newNode = (NODE *)malloc(sizeof(NODE));
+    if (newNode==NULL) {
+        printf("/nNODE CREATION FAILED");
+        return;
+    }
+    newNode->data=item;
+    newNode->next=NULL;
+    NODE *current = head;
+    while (current->next!=NULL) {
+        current=current->next;
+    }
+    current->next=newNode;
+}
+
+void deleteFromTheEnd(){
+    if (head==NULL) {
+        printf("\nLL is empty, nothing to delete");
+        return;
+    }
+    NODE *previous = NULL;
+    NODE *current = head;
+    while (current->next!=NULL) {
+        previous=current;
+        current=current->next;
+    }
+    previous->next=NULL;
+    free(current);
+    
+}
+
+void insertItemAtPostion(int item, int position)
+{
+    
+    if (position<=1) {
+        insertAtTheBeginning(item);
+        return;
+    }
+    NODE *newNode = (NODE *)malloc(sizeof(NODE));
+    if (newNode==NULL) {
+        printf("/nNODE CREATION FAILED");
+        return;
+    }
+    newNode->data=item;
+    int k=1;
+    NODE *current = head;
+    NODE *previous=NULL;
+    while (k<position && current!=NULL) {
+        k++;
+        previous=current;
+        current=current->next;
+    }
+    previous->next=newNode;
+    newNode->next=current;
 }
